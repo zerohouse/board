0a4788eccb1802071c340fde8c50bc5c68ad57f3
dev <parksungho86@gmail.com>
2015-06-15 21:32:25 +0900

    init

---
+++ src/begin/launcher/DBLauncher.java
@@ -0,0 +1,10 @@
+package begin.launcher;
+
+import next.jdbc.mysql.maker.PackageCreator;
+
+public class DBLauncher {
+	public static void main(String[] args) throws Exception {
+		PackageCreator.reset();
+	}
+
+}
